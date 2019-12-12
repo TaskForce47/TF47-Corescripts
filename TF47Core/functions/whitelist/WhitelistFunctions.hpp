6e98c59e0629f1bffd0fe33d38a5b20c74f3ec68
Oliver <oliverkrue@googlemail.com>
2019-12-13 00:17:15 +0100

    added functions to function lib

--- TF47Core/functions/whitelist/WhitelistFunctions.hpp
+++ TF47Core/functions/whitelist/WhitelistFunctions.hpp
@@ -2,6 +2,12 @@ class TF47_whitelist
 {
 	class whitelist 
 	{
-
+		file = "TF47Core\functions\whitelist";
+		class addWhitelistSlot {};
+		class checkSlotWhitelist {};
+		class checkVehicleWhitelist {};
+		class checkWhitelist {};
+		class init {};
+		class reloadWhitelists {};
 	};
 };
