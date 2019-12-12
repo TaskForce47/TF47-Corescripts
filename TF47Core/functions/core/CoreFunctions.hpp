b207b29b8dd6c392246228e72c9b1d723a02182c
Oliver <oliverkrue@googlemail.com>
2019-12-12 19:40:35 +0100

    renamed functions to prevent irritation with bi functions

--- TF47Core/functions/core/CoreFunctions.hpp
+++ TF47Core/functions/core/CoreFunctions.hpp
@@ -3,15 +3,14 @@ class TF47_database
 	class database
 	{
 		file = "TF47Core\functions\core";
-		class createPlayer {};
 
 		class handleConnectionHC {};
 		class handleDisconnectHC {};
-
+		class handlePlayerConnected {};
+		class handlePlayerDisconnected {};
 		class init {postInit = 1;};
 
-		class onPlayerConnected {};
-		class onPlayerDisconnected {};
+		
 		class settings {preInit = 1;};
 		class setupDatabase {};
 	};
