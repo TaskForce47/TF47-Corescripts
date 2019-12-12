e1093226c99bc1d65eb40b1cc2cf8d75e99a1f9d
Oliver <oliverkrue@googlemail.com>
2019-12-13 00:17:47 +0100

    moved Whitelist id marcos to a wider scope

--- TF47Core/functions/whitelist/script_component.hpp
+++ TF47Core/functions/whitelist/script_component.hpp
@@ -4,13 +4,4 @@
 
 #define DEBUG_MODE_FULL
 
-#define WHITELIST_CCT 1
-#define WHITELIST_HELO 2
-#define WHITELIST_PLANE 3
-#define WHITELIST_UAV 4
-#define WHITELIST_TANK 5
-#define WHITELIST_BUILDERS 6
-#define WHITELIST_ISMODERATOR 7
-#define WHITELIST_ISADMIN 8
-
 #include "..\..\script_macros_common.hpp"
