eb85cea70eb3d7e768ba86cf3fc4748bc8a3dcc5
Oliver <oliverkrue@googlemail.com>
2019-12-18 19:57:21 +0100

    use new paa files

--- TF47Core/CfgNotification.hpp
+++ TF47Core/CfgNotification.hpp
@@ -11,30 +11,30 @@ class TF47_core_notification {
 
 class TF47_core_notification_ticketLoss : TF47_core_notification {
 	title = "Ticket loss";
-	iconPicture = "TF47Core\data\icons\minus.paa";
+	iconPicture = "TF47Core\data\icons\tickets_loss.paa";
 	description = "%1";
 };
 
 class TF47_core_notification_ticketGain : TF47_core_notification {
 	title = "Ticket gain";
-	iconPicture = "TF47Core\data\icons\plus.paa";
+	iconPicture = "TF47Core\data\icons\tickets_gain.paa";
 	description = "%1";
 };
 
 class TF47_core_notification_outOfTickets : TF47_core_notification {
 	title = "No tickets remaining";
-	iconPicture = "TF47Core\data\icons\fail.paa";
+	iconPicture = "TF47Core\data\icons\tickets_lost.paa";
 	description = "%1";
 };
 
 class TF47_core_notification_notWhitelisted : TF47_core_notification {
 	title = "Not whitelisted";
-	iconPicture = "TF47Core\data\icons\not_allowed.paa";
+	iconPicture = "TF47Core\data\icons\wrong_slot.paa";
 	description = "%1";
 };
 
 class TF47_core_notification_badWeapon : TF47_core_notification {
 	title = "You are not allowed to carry that weapon";
-	iconPicture = "TF47Core\data\icons\not_allowed_weapon.paa";
+	iconPicture = "TF47Core\data\icons\bad_weapon.paa";
 	description = "%1";
 };
