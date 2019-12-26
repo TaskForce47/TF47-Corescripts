ff12f0ea6c979bfd440b9efc0bd914ea3d69d3cf
Oliver Krüppel <oliverkrue@googlemail.com>
2019-12-26 01:47:10 +0100

    moved folder ticketsys  and added functions to lib

--- TF47Core/CfgFunctions.hpp
+++ TF47Core/CfgFunctions.hpp
@@ -1,5 +1,5 @@
 #include "functions\core\CoreFunctions.hpp"
 #include "functions\logging\LoggingFunctions.hpp"
-#include "functions\ticketsystem\TicketsystemFunctions.hpp"
+#include "functions\ticketsys\TicketsystemFunctions.hpp"
 #include "functions\marker\MarkerFunctions.hpp"
 #include "functions\whitelist\WhitelistFunctions.hpp"
