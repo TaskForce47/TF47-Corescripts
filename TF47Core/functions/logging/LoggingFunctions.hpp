5d99d5bd72e8adf60dbbad374e10780fb4af74ca
Oliver <oliverkrue@googlemail.com>
2020-05-10 02:43:05 +0200

    added new placeholder functions

--- TF47Core/functions/logging/LoggingFunctions.hpp
+++ TF47Core/functions/logging/LoggingFunctions.hpp
@@ -1,9 +1,13 @@
-class TF47_logging 
+class TF47_logging
 {
 	class logging
 	{
 		file = "TF47Core\functions\logging";
+		class chatLogger {};
 		class initPlayer {};
 		class initServer {};
+		class killTracker {};
+		class medicalTracker {};
+		class positionTracking {};
 	};
 };
