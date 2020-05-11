b929769fde18e1cb171bcdfd9c46803b674a2297
Oliver <oliverkrue@googlemail.com>
2020-05-11 19:06:58 +0200

    add vehicle flipping

---
+++ TF47Core/unflipping/UnflippingFunctions.hpp
@@ -0,0 +1,24 @@
+class vet_unflipping
+{
+  class functions
+  {
+    file = "TF47Core\unflipping\functions";
+
+    class addUnflipActionLocal {};
+    class canUnflipLocal {};
+    class hasToolKitRequired {};
+    class isRepairVehicle {};
+    class isServiceVehicleNearRequired {};
+    class unflipAction {};
+    class unflipRequiredAmount {};
+    class unflipVehicle {};
+    class initAction {};
+    class initSettings {preInit=1};
+  };
+
+  class debug
+  {
+    file = "TF47Core\unflipping\functions\debug";
+    class debug {};
+  };
+};
